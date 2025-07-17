c0b0f9459a2a1d9242388c8c041e612cb48191a6
firstrepo <sagarkharal024@gmail.com>
2025-07-17 10:27:17 +0530

    done setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <>
+   <h1 className='text-5xl text-center text-lime-400 font-extrabold'>
+    Hello there
+   </h1>
+    </>
+  )
+}
+
+export default App
